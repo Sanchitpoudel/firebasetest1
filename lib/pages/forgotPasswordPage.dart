2e13ed5cef530b1488cdaeacc2952c74b23884d5
Sanchit Poudel <snchtpdl12@gmail.com>
2023-01-20 16:02:01 +0545

    Forgot password done

--- lib/pages/forgotPasswordPage.dart
+++ lib/pages/forgotPasswordPage.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: camel_case_types
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class forgotPasswordPage extends StatefulWidget {
   const forgotPasswordPage({super.key});
@@ -10,15 +13,96 @@ class forgotPasswordPage extends StatefulWidget {
 }
 
 class _forgotPasswordPageState extends State<forgotPasswordPage> {
+  TextEditingController _emailController = TextEditingController();
+
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(
+            'Password reset link sent! Check your email.'
+            ),
+          );
+        },
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(
+              e.message.toString(),
+            ),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/keys.png',height: 30,),
-          Text('Forgot password?'),
-        ],
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Forgot password?',
+              style: GoogleFonts.montserrat(
+                  fontSize: 28,
+                  color: Color.fromARGB(255, 21, 20, 20),
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              'No worries, We\'ll send you email.',
+              style: GoogleFonts.montserrat(
+                  color: Colors.grey[600], fontWeight: FontWeight.w500),
+            ),
+            //sized box
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+              ),
+            ),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6)),
+              onPressed: passwordReset,
+              color: Colors.deepPurple,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Reset Password',
+                  style: GoogleFonts.montserrat(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
